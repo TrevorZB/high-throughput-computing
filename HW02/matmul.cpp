97d6c43d29aedd95781ea9241ec610873eb57814
trevor <trevorzb11@gmail.com>
2021-02-07 21:25:08 -0600

    more work needed task3

--- HW02/matmul.cpp
+++ HW02/matmul.cpp
@@ -1,7 +1,7 @@
 #include "matmul.h"
 
 // using for testing purposes
-#include <iostream>
+// #include <iostream>
 
 void mmul1(const double* A, const double* B, double* C, const unsigned int n)
 {
@@ -63,15 +63,15 @@ void mmul4(const std::vector<double>& A, const std::vector<double>& B, double* C
     }
 }
 
-int main()
-{
-    const double A[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    const double B[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
-    double C[9] = {};
-    mmul2(A, B, C, 3);
-    for (std::size_t i = 0; i < 9; i++)
-    {
-        std::cout << C[i] << ", ";
-    }
-    std::cout << std::endl;
-}
+// int main()
+// {
+//     const double A[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+//     const double B[]{1, 2, 3, 4, 5, 6, 7, 8, 9};
+//     double C[9] = {};
+//     mmul2(A, B, C, 3);
+//     for (std::size_t i = 0; i < 9; i++)
+//     {
+//         std::cout << C[i] << ", ";
+//     }
+//     std::cout << std::endl;
+// }
---
+++ HW02/task3.cpp
@@ -0,0 +1,64 @@
+#include "matmul.h"
+#include <iostream>
+#include <stdlib.h>
+#include <random>
+#include <chrono>
+
+using std::chrono::high_resolution_clock;
+using std::chrono::duration;
+
+void time_test(const double* A, 
+               const double* B, 
+               const unsigned int n,
+               void (*mmul_func)(
+                   const double* A,
+                   const double* B,
+                   double* C,
+                   const unsigned int n
+               ))
+{
+    double *C = new double[n * n];
+    high_resolution_clock::time_point start;
+    high_resolution_clock::time_point end;
+    duration<double, std::milli> duration_sec;
+
+    start = high_resolution_clock::now();
+    (*mmul_func)(A, B, C, n);
+    end = high_resolution_clock::now();
+    duration_sec = std::chrono::duration_cast<duration<double, std::milli>>(end - start);
+
+    std::cout << duration_sec.count() << "\n";
+    std::cout << C[(n * n) - 1] << "\n";
+
+    delete[] C;
+
+}
+
+int main()
+{
+    unsigned int n = 1024;
+
+    double *A = new double[n * n];
+    double *B = new double[n * n];
+
+    std::default_random_engine gen{static_cast<long unsigned int>(time(0))};
+    std::uniform_real_distribution<double> dist(1.0, 10.0);
+
+    for (std::size_t i = 0; i < (n * n); i++)
+    {
+        A[i] = dist(gen);
+    }
+    for (std::size_t i = 0; i < (n * n); i++)
+    {
+        B[i] = dist(gen);
+    }
+
+    std::cout << n << "\n";
+    time_test(A, B, n, mmul1);
+    time_test(A, B, n, mmul2);
+    time_test(A, B, n, mmul3);
+        
+
+    delete[] A;
+    delete[] B;
+}
